603cdca1629fc8e14ed0999fa395595a3111e003
Lukathis <xuchi16@126.com>
2025-09-05 17:16:51 +0800

    feat: refactor structure

---
+++ LowLevelMesh/InteractiveGeometry/InteractiveGeometryTests/InteractiveGeometryTests.swift
@@ -0,0 +1,12 @@
+// Created by Chester for InteractiveGeometry in 2025
+
+import Testing
+@testable import InteractiveGeometry
+
+struct InteractiveGeometryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
